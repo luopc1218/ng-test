e3d1a10ae08e5dba1e83f2e585607a3a0379ac75
luopeichun <luopc1218@outlook.com>
2020-06-10 18:07:54 +0800

    修改首页样式 添加注销逻辑

--- src/app/pages/home/navigator/navigator.component.ts
+++ src/app/pages/home/navigator/navigator.component.ts
@@ -1,5 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 
+interface ModuleItem {
+  id: number;
+  title: string;
+  name: string;
+  children?: ModuleItem[];
+}
+
 @Component({
   selector: 'app-navigator',
   templateUrl: './navigator.component.html',
@@ -7,13 +14,31 @@ import {Component, OnInit} from '@angular/core';
 })
 export class NavigatorComponent implements OnInit {
 
+  moduleList: ModuleItem[];
 
   constructor() {
   }
 
   ngOnInit(): void {
-
+    this.getModuleList()
   }
 
+  //  获取模块列表
+  getModuleList(): void {
+    this.moduleList = [
+      {
+        id: 1, title: '模块1', name: 'module1', children: [
+          {
+            id: 11, title: '模块11', name: 'module11'
+          },
+          {
+            id: 12, title: '模块12', name: 'module12'
+          }
+        ]
+      },
+      {id: 2, title: '模块1', name: 'module1'}
+    ]
+    console.log(this.moduleList)
+  }
 
 }
