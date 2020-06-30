d1ab0ddb64e5ccbd29a4780a41dbfb4c8974f5ca
luopeichun <luopc1218@outlook.com>
2020-06-30 18:05:33 +0800

    添加md5和注册相关表单

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,54 +1,55 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HttpClientModule} from '@angular/common/http';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NZ_I18N, zh_CN} from 'ng-zorro-antd/i18n';
-import {registerLocaleData} from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N, zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
 //  NzComponents
-import {NzIconModule} from 'ng-zorro-antd/icon';
-import {NzInputModule} from 'ng-zorro-antd/input';
-import {NzButtonModule} from 'ng-zorro-antd/button';
-import {NzFormModule} from 'ng-zorro-antd/form';
-import {NzPageHeaderModule} from 'ng-zorro-antd/page-header';
-import {NzMessageModule} from 'ng-zorro-antd/message';
-import {NzLayoutModule} from 'ng-zorro-antd/layout';
-import {NzMenuModule} from 'ng-zorro-antd/menu';
-import {NzSpinModule} from 'ng-zorro-antd/spin';
-import {NzModalModule} from 'ng-zorro-antd/modal';
-import {NzResultModule} from 'ng-zorro-antd/result';
-import {NzSelectModule} from 'ng-zorro-antd/select';
-import {NzTableModule} from 'ng-zorro-antd/table';
-import {NzPopconfirmModule} from 'ng-zorro-antd/popconfirm';
-import {NzBreadCrumbModule} from 'ng-zorro-antd/breadcrumb';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzResultModule } from 'ng-zorro-antd/result';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
 //  NzIcons
 import * as AllIcons from '@ant-design/icons-angular/icons';
-import {IconDefinition} from '@ant-design/icons-angular';
+import { IconDefinition } from '@ant-design/icons-angular';
 //  pages
-import {LoginComponent} from './pages/login/login.component';
-import {HomeComponent} from './pages/home/home.component';
-import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
-import {LoginFormComponent} from './pages/login/login-form/login-form.component';
-import {LoginRegisteredComponent} from './pages/login/login-registered/login-registered.component';
-import {NavigatorComponent} from './pages/home/navigator/navigator.component';
-import {HeaderComponent} from './pages/home/header/header.component';
-import {Test11Component} from './pages/home/children/test11/test11.component';
-import {Test12Component} from './pages/home/children/test12/test12.component';
-import {Test2Component} from './pages/home/children/test2/test2.component';
-import {EditUserInfoComponent} from './pages/home/children/test11/edit-user-info/edit-user-info.component';
-import {BreadcrumbComponent} from './pages/home/breadcrumb/breadcrumb.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { LoginFormComponent } from './pages/login/login-form/login-form.component';
+import { LoginRegisteredComponent } from './pages/login/login-registered/login-registered.component';
+import { NavigatorComponent } from './pages/home/navigator/navigator.component';
+import { HeaderComponent } from './pages/home/header/header.component';
+import { Test11Component } from './pages/home/children/test11/test11.component';
+import { Test12Component } from './pages/home/children/test12/test12.component';
+import { Test2Component } from './pages/home/children/test2/test2.component';
+import { EditUserInfoComponent } from './pages/home/children/test11/edit-user-info/edit-user-info.component';
+import { BreadcrumbComponent } from './pages/home/breadcrumb/breadcrumb.component';
 
 // enableProdMode();
-
 
 const antDesignIcons = AllIcons as {
   [key: string]: IconDefinition;
 };
-const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key]);
-
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(
+  (key) => antDesignIcons[key]
+);
 
 registerLocaleData(zh);
 
@@ -91,10 +92,10 @@ registerLocaleData(zh);
     NzSelectModule,
     NzTableModule,
     NzPopconfirmModule,
-    NzBreadCrumbModule
+    NzBreadCrumbModule,
+    NzRadioModule,
   ],
-  providers: [{provide: NZ_I18N, useValue: zh_CN}],
-  bootstrap: [AppComponent]
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/pages/login/login-registered/login-registered.component.ts
+++ src/app/pages/login/login-registered/login-registered.component.ts
@@ -1,7 +1,14 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {Router} from '@angular/router';
-import {ConfigService} from '../../../config.service';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { ConfigService } from '../../../config.service';
+//  !MD5
+import { Md5 } from 'ts-md5/dist/md5';
 
 @Component({
   selector: 'app-login-registered',
@@ -12,27 +19,47 @@ export class LoginRegisteredComponent implements OnInit {
   registeredForm!: FormGroup;
   loading = false;
 
-  constructor(private fb: FormBuilder, private router: Router, private request: ConfigService) {
-  }
-
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private request: ConfigService
+  ) {}
+  //  验证两次密码输入是否一致
+  confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { required: true };
+    } else if (control.value !== this.registeredForm.controls.password.value) {
+      return { valueInvalid: true, error: true };
+    }
+    return {};
+  };
   ngOnInit(): void {
-    this.registeredForm = this.fb.group({});
+    this.registeredForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      passwordCheck: [null, [Validators.required, this.confirmationValidator]],
+      sex: [null],
+      email: [null, [Validators.email, Validators.required]],
+      phone: [null],
+    });
   }
 
   submit(): void {
     this.loading = true;
-    this.request.get({
-      url: '/test',
+    console.log(this.registeredForm.value);
+    this.request.post({
+      url: '/users/add',
       params: {
-        test: 'test',
-        test2: 'test2'
+        userName: this.registeredForm.value.userName,
+        password: Md5.hashStr(this.registeredForm.value.password),
+        email: this.registeredForm.value.email,
       },
       success: (res) => {
         console.log(res);
       },
       complete: () => {
         this.loading = false;
-      }
+      },
     });
   }
 
